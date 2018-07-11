7180562610bc60ed2460016c13d1f2d5e79ca1fb
Nuno Santos <nuno_913@hotmail.com>
2018-07-11 18:45:31 +0100

    Added request for storage permission

--- app/src/main/java/pt/ulisboa/tecnico/cmov/triviawinner/MainActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/triviawinner/MainActivity.java
@@ -1,11 +1,15 @@
 package pt.ulisboa.tecnico.cmov.triviawinner;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -15,12 +19,22 @@ import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends Activity {
     private static final int CODE_DRAW_OVER_OTHER_APP_PERMISSION = 2084;
+    public static final int PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 968;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+        }
 
         //Check if the application has draw over other apps permission or not?
         //This permission is by default available for API<23. But for API > 23
@@ -63,5 +77,15 @@ public class MainActivity extends Activity {
         i.putExtra(Constants.GAME_LANG,language);
         startService(i);
         finish();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE: {
+                return;
+            }
+        }
     }
 }
